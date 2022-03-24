7066abd97e66c293431460a6c26e0b041bc6b800
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-03-24 17:47:40 +0100

    sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,93 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+/**
+ * Challenge: Connect the form to local state
+ *
+ * 1. Create a state object to store the 4 values we need to save.
+ * 2. Create a single handleChange function that can
+ *    manage the state of all the inputs and set it up
+ *    correctly
+ * 3. When the user clicks "Sign up", check if the
+ *    password & confirmation match each other. If
+ *    so, log "Successfully signed up" to the console.
+ *    If not, log "passwords to not match" to the console.
+ * 4. Also when submitting the form, if the person checked
+ *    the "newsletter" checkbox, log "Thanks for signing
+ *    up for our newsletter!" to the console.
+ */
+export default function App() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isSubscribed: false,
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { password, confirmPassword, isSubscribed } = formData;
+    password === confirmPassword
+      ? console.log("Successfully signed up")
+      : console.error("Passwords does not match");
+    isSubscribed
+      ? console.log("Thanks for signing up for our newsletter!")
+      : null;
+    console.log(formData);
+  }
+  function handleChange(event) {
+    const { name, type, checked, value } = event.target;
+    // estas propiedades las desestructuramos del event.target porque nos interesa conocer
+    // qué objeto ha hecho click y que valor o check tiene almacenado
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+    console.log(formData);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email address"
+          className="form--input"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form--input"
+          name="password"
+          onChange={handleChange}
+          value={formData.password}
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          className="form--input"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
 
-export default App
+        <div className="form--marketing">
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            name="isSubscribed"
+            onChange={handleChange}
+            checked={formData.isSubscribed}
+          />
+          <label htmlFor="okayToEmail">I want to join the newsletter</label>
+        </div>
+        <button className="form--submit">Sign up</button>
+      </form>
+    </div>
+  );
+}
